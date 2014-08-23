5018edc8e6937fbf385a138506db25c4ee1c8547
ibillius <wss.simpson@gmail.com>
2014-08-23 00:29:22 -0700

    Added basic MongoDB query via AJAX to populate graph.

---
+++ edges.js
@@ -0,0 +1,9 @@
+{"_id": "chris_christy-mark_zuckerberg", "source": "chris_christy", "target": "mark_zuckerberg"}
+
+{"_id": "mark_zuckerberg-bill_gates", "source": "mark_zuckerberg", "target": "bill_gates"}
+{"_id": "mark_zuckerberg-sheryl_sandberg", "source": "mark_zuckerberg", "target": "sheryl_sandberg"}
+{"_id": "mark_zuckerberg-reed_hastings", "source": "mark_zuckerberg", "target": "reed_hastings"}
+
+{"_id": "bill_gates-elon_musk", "source": "bill_gates", "target": "elon_musk"}
+{"_id": "bill_gates-ryan_seacrest", "source": "bill_gates", "target": "ryan_seacrest"}
+{"_id": "bill_gates-chris_anderson", "source": "bill_gates", "target": "chris_anderson"}
---
+++ nodes.js
@@ -0,0 +1,9 @@
+{"_id": "chris_christy", "name": "Chris Christy", "participated": 1}
+{"_id": "mark_zuckerberg", "name": "Mark Zuckerberg", "participated": 1}
+{"_id": "sheryl_sandberg", "name": "Sheryl Sandberg", "participated": 0}
+{"_id": "reed_hastings", "name": "Reed Hastings", "participated": 0}
+
+{"_id": "bill_gates", "name": "Bill Gates","participated": 1}
+{"_id": "elon_musk", "name": "Elon Musk","participated": 0}
+{"_id": "ryan_seacrest", "name": "Ryan Seacrest","participated": 0}
+{"_id": "chris_anderson", "name": "Chris Anderson","participated": 0}
